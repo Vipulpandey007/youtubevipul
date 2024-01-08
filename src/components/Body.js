d475d5c21df8002f631af5590990368b9c6725f0
Vipulpandey007 <vipulpandey708@gmail.com>
2024-01-08 20:41:33 +0530

    Top Loading bar added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 
 const Body = ({ setProgress }) => {
+  const param = useParams();
   useEffect(() => {
     setProgress(30);
     setTimeout(() => {
       setProgress(100);
     }, 1500);
-  }, []);
+  }, [param]);
   return (
     <div className="flex flex-row h-[calc(100%-56px)]">
       <Sidebar />
