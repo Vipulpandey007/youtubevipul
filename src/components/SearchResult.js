54af18e37e6fc5a3f895d31805599c9accbb9c5f
Vipulpandey007 <vipulpandey708@gmail.com>
2024-01-30 10:54:43 +0530

    search result component fixed

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -23,23 +23,25 @@ const SearchResult = () => {
     console.log(searchedVideosJson?.items?.id);
   };
   return (
-    <div className="px-3 col-span-11 mt-10">
-      <div className="flex flex-col px-3  items-center">
-        <div className="p-2 m-2 mt-10">
+    <div className="md:px-3 col-span-11 ">
+      <div className="flex flex-col md:px-3  items-center">
+        <div className="md:p-2 md:ml-[250px]">
           {searchedVideos?.map((video) => (
             <Link
               key={video?.id?.videoId}
               to={"/watch?v=" + video?.id?.videoId}
             >
-              <div className="px-3 m-4 flex">
+              <div className="md:px-3 m-4 flex flex-col sm:flex-row ">
                 <img
-                  className="rounded-lg w-[350px] h-[200px] mt-4"
+                  className="rounded-lg  w-full md:w-[300px] h-[200px] mt-4"
                   alt="thumbnail"
                   src={video?.snippet?.thumbnails?.medium?.url}
                 />
-                <ul className="flex flex-col justify-start ml-5 w-96">
-                  <li className=" py-2 text-xl ">{video?.snippet?.title}</li>
-                  <li className="text-gray-500 text-[18px]">
+                <ul className="flex flex-col justify-start ml-5 mt-2 w-[90%] md:w-full">
+                  <li className="text-sm lg:text-xs xl:text-sm font-bold line-clamp-2 text-black">
+                    {video?.snippet?.title}
+                  </li>
+                  <li className="text-[12px] lg:text-[10px]  xl:text-[12px] font-semibold mt-2 text-black/[0.7] flex items-center">
                     {video?.snippet?.channelTitle}
                   </li>
                   <li className="text-gray-500 text-[18px]">
@@ -52,7 +54,7 @@ const SearchResult = () => {
                     ).toFixed(1)}{" "}
                     days ago
                   </li>
-                  <li className="text-gray-500 mt-2 text-[15px]">
+                  <li className="text-gray-500 mt-2 text-[15px] hidden sm:block">
                     {video?.snippet?.description}
                   </li>
                 </ul>
